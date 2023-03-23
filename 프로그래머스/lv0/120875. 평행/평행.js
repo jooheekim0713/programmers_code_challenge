7900d45f1f200a159541e81ab405d908de3778b8
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-03-23 18:09:12 +0900

    [level 0] Title: 평행, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.js
+++ 프로그래머스/lv0/120875. 평행/평행.js
@@ -1,25 +1,13 @@
 function solution(dots) {
-  var answer = 0;
+    if (calculateSlope(dots[0], dots[1]) === calculateSlope(dots[2], dots[3]))
+        return 1;
+    if (calculateSlope(dots[0], dots[2]) === calculateSlope(dots[1], dots[3]))
+        return 1;
+    if (calculateSlope(dots[0], dots[3]) === calculateSlope(dots[1], dots[2]))
+        return 1;
+    return 0;
+}
 
-  function calculation(a, b, c, d) {
-    let abDiff, cdDiff;
-
-    abDiff = (b[1] - a[1]) / (b[0] - a[0]);
-    cdDiff = (d[1] - c[1]) / (d[0] - c[0]);
-
-    if (abDiff === cdDiff) {
-      answer += 1;
-    }
-  }
-
-  // 1-2, 3-4가 이어졌을 때
-  calculation(dots[0], dots[1], dots[2], dots[3]);
-
-  // 1-3, 2-4가 이어졌을 때
-  calculation(dots[0], dots[2], dots[1], dots[3]);
-
-  // 1-4, 2-3가 이어졌을 때
-  calculation(dots[0], dots[3], dots[1], dots[2]);
-
-  return answer > 0 ? 1 : 0;
+function calculateSlope(arr1, arr2) {
+    return (arr2[1] - arr1[1]) / (arr2[0] - arr1[0]);
 }
