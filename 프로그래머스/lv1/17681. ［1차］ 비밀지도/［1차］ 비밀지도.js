6e668680937369852553aadf6374cf705ac778f7
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-04-04 15:25:47 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,22 @@
+function solution(n, arr1, arr2) {
+  const answer = [];
+  const sumArr = [];
+   
+  const A = arr1.map((num) => num.toString(2).padStart(n, 0));
+  const B = arr2.map((num) => num.toString(2).padStart(n, 0));
+  
+  for (let i = 0; i < n; i++) {
+    sumArr.push(String(+A[i] + +B[i]).padStart(n, 0));
+  }
+    
+  for (let num of sumArr) {
+    answer.push(
+      num
+        .split("")
+        .map((item) => (item === "0" ? (item = " ") : (item = "#")))
+        .join("")
+    );
+  }
+  
+  return answer;
+}
