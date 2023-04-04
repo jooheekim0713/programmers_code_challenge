fc308f537fbbf31cc3714a90148a11f30b7d88ee
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-04-04 15:27:38 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,22 +1,7 @@
 function solution(n, arr1, arr2) {
-  const answer = [];
-  const sumArr = [];
-   
-  const A = arr1.map((num) => num.toString(2).padStart(n, 0));
-  const B = arr2.map((num) => num.toString(2).padStart(n, 0));
-  
-  for (let i = 0; i < n; i++) {
-    sumArr.push(String(+A[i] + +B[i]).padStart(n, 0));
-  }
-    
-  for (let num of sumArr) {
-    answer.push(
-      num
-        .split("")
-        .map((item) => (item === "0" ? (item = " ") : (item = "#")))
-        .join("")
-    );
-  }
-  
-  return answer;
+    return arr1.map((v, i) => addZero(n, (v | arr2[i]).toString(2)).replace(/1|0/g, a => +a ? '#' : ' '));
+}
+
+const addZero = (n, s) => {
+    return '0'.repeat(n - s.length) + s;
 }
