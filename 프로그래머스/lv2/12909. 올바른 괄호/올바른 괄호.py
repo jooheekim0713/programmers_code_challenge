860982f107f1b45ead128c09b538f45582aa264a
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-06-23 20:34:06 +0900

    [level 2] Title: 올바른 괄호, Time: 8.64 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = True
+    stack = []
+    for i in s:
+        if i == '(':
+            stack.append('(')
+        else:
+            if stack == []:
+                return False
+            else:
+                stack.pop()
+    if stack != []:
+        return False
+    return True
