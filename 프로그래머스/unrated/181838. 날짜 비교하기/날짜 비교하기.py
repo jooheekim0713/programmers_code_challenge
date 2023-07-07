ff5e52c55a290ed6470ad5db05670b87f84a0e00
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-07-07 13:46:57 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+def solution(date1, date2):
+    answer = 0
+    first = datetime(date1[0],date1[1],date1[2])
+    second = datetime(date2[0],date2[1],date2[2])
+    return 1 if first < second else 0
