e60d78fe7f4e512767c9c7e45028bfc490a6ded0
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-07-07 10:59:02 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,6 +1,8 @@
 def solution(arr, idx):
-    arr1 = arr[idx:]
-    if 1 in arr1:
-        return arr1.index(1) + idx
-    else:
-        return -1
+    answer = 0
+    try:
+        answer = arr.index(1, idx)
+    except:
+        answer = -1
+
+    return answer
