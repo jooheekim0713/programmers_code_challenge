e3bd2b783b162987f0ac44c616bad9c91c5b1add
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-07-07 12:50:46 +0900

    [unrated] Title: 수 조작하기 2, Time: 13.05 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,14 +1,14 @@
 def solution(numLog):
     answer = ''
-    for idx,v in enumerate(numLog):
-        if idx > 0:
-            num = numLog[idx]-numLog[idx-1]
-            if num == 1:
-                answer += 'w'
-            elif num == -1:
-                answer += 's'
-            elif num == 10:
-                answer += 'd'
-            elif num == -10:
-                answer += 'a'
+    for i in range(1, len(numLog)):
+        diff = numLog[i] - numLog[i-1] # 현재 값과 이전 값의 차이를 계산
+        if diff == 1:
+            answer += 'w' # 1 더하기
+        elif diff == -1:
+            answer += 's' # 1 빼기
+        elif diff == 10:
+            answer += 'd' # 10 더하기
+        elif diff == -10:
+            answer += 'a' # 10 빼기
     return answer
+
