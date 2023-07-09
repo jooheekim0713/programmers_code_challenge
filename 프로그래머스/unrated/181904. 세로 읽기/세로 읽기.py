76f4ea9301b89e36ad63719167d2ea557bfb5bf4
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-07-09 16:24:15 +0900

    [unrated] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
@@ -1,5 +1,2 @@
-def solution(my_string, m, c):
-    answer = ''
-    for i in range(int(len(my_string)/m)):
-        answer += my_string[i*m+c-1]
-    return answer
+def solution(s, m, c):
+    return s[c-1::m]
