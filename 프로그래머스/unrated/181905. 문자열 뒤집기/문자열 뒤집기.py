685bb0121219325e008b082dce8e2dbbf30a35f8
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-07-26 19:50:55 +0900

    [unrated] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,6 +1,2 @@
 def solution(my_string, s, e):
-    answer = my_string[:s]
-    reverse_str = ''
-    for c in my_string[s:e+1]:
-        reverse_str = c + reverse_str
-    return answer+reverse_str+my_string[e+1:]
+    return my_string[:s]+my_string[s:e+1][::-1]+my_string[e+1:]
