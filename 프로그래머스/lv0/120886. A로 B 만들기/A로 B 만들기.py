c02acf23bd671c49892fd6823f6fd74c4c0ea600
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-07-27 13:36:32 +0900

    [level 0] Title: A로 B 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,8 +1,2 @@
 def solution(before, after):
-    answer = 0
-    list_b = list(before)
-    list_a = list(after)
-    for i in list_b:
-        if i in list_a:
-            list_a.remove(i)
-    return 1 if len(list_a) == 0 else 0
+    return 1 if sorted(before) == sorted(after) else 0
