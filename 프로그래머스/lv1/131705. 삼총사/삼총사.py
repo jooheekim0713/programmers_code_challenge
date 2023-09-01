df48f7505cd34d29b8d5a0ac547fab38416545b5
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-09-01 16:17:54 +0900

    [level 1] Title: 삼총사, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,9 +1,8 @@
-def solution(number):
-    answer = 0
-    l = len(number)
-    for i in range(l-2):
-        for j in range(i+1, l-1):
-            for k in range(j+1, l):
-                if number[i]+number[j]+number[k] == 0:
-                    answer += 1           
-    return answer
+from itertools import combinations
+
+def solution (number) :
+    cnt = 0
+    for i in combinations(number,3) :
+        if sum(i) == 0 :
+            cnt += 1
+    return cnt
