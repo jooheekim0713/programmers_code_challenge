127d83cd2237aa272e804c05232d363e9c805644
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-09-07 15:00:08 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,13 +1,9 @@
 def solution(k, score):
     answer = []
-    k_scores = []
-    
-    for i in score:
-        k_scores.append(i)
-        k_scores.sort(reverse=True)
-        
-        if len(k_scores) > k:
-            k_scores.pop()
-            
-        answer.append(k_scores[-1])
+    honor = []
+    for each in score:
+        honor.append(each)
+        if len(honor) > k:
+            honor.remove(min(honor))
+        answer.append(min(honor))
     return answer
