47bd431fa4e775aa18eb5f28f4f97e001e129f2d
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-09-07 10:15:45 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/README.md
+++ 프로그래머스/lv1/12977. 소수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 소수 만들기 - 12977 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12977?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12977) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.02 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -1,11 +1,12 @@
-from itertools import combinations
-
-def prime_number(x):
-    answer = 0
-    for i in range(1,int(x**0.5)+1):
-        if x%i == 0:
-            answer += 1
-    return 1 if answer == 1 else 0
-
 def solution(nums):
-    return sum([prime_number(sum(c)) for c in combinations(nums,3)])
+    answer = 0
+    l = len(nums)
+    
+    for i in range(l):
+        for j in range(i+1, l):
+            for k in range(j+1, l):
+                number = nums[i] + nums[j] + nums[k]
+                if len([m for m in range(2, number) if number % m == 0]) == 0:
+                    answer += 1
+
+    return answer
