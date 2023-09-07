4d2c417755f74fe03c4f9712a1052734551646f0
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-09-07 19:09:34 +0900

    [unrated] Title: 기사단원의 무기, Time: 7.90 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -6,14 +6,8 @@ def getDivisor(n):
             divisorsList.append(i) 
             if ( (i**2) != n) : 
                 divisorsList.append(n // i)
-                
+
     return len(divisorsList)
 
 def solution(number, limit, power):
-    answer = 0
-    list_a = [x+1 for x in range(number)]
-    for a in list_a:
-        div_res = getDivisor(a)
-        answer += power if limit < div_res else div_res
-            
-    return answer
+    return sum([getDivisor(i) if getDivisor(i) <= limit else power for i in range(number+1)])
