e29c9740edfd9aec88bc9e827e9655a18faae62a
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-09-08 09:05:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 50.06 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,9 +1,12 @@
 def solution(participant, completion):
-    completion.sort()
-    participant.sort()
-    
-    for i,val in enumerate(completion):
-        if participant[i] != completion[i]:
-            return participant[i]
-    
-    return  participant[-1]
+    answer = ''
+    temp = 0
+    dic = {}
+    for part in participant:
+        dic[hash(part)] = part
+        temp += int(hash(part))
+    for com in completion:
+        temp -= hash(com)
+    answer = dic[temp]
+
+    return answer
