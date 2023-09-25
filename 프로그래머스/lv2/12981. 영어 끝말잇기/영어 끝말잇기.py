5b1b162560cb332de6664023e795d199ea84e8a8
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-09-25 15:11:04 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.18 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,22 +1,14 @@
-def solution(n, words):
-    answer = []
-    turn = 0
-    wordList = [words[0]]
-    
-    for idx in range(1, len(words)):
-        if words[idx-1][-1] != words[idx][0]:
-            turn = idx
-            break
-            
-        if words[idx] in wordList:
-            turn = idx
-            break
-            
-        wordList.append(words[idx])
-        
-    answer = [turn%n +1, turn//n + 1]
-    
-    if turn == 0:
-        answer = [0, 0]
-        
-    return answer
+def solution(num, lst):
+    elst = []
+    cnt = 0
+    for i in range(1, len(lst)):
+        ans = i // num +1 
+        ans2 = i % num + 1
+        if lst[i] in lst[:i]:
+            print("ch")
+            return [ans2, ans]
+        elif lst[i][0] != lst[i-1][-1]:
+            print("ch", i)
+            return [ans2, ans]
+    else:
+        return [0,0]
