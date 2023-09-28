9346fe39c8187b2a6b0d420525e68acaa7fa92d9
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-09-28 20:45:37 +0900

    [level 2] Title: n^2 배열 자르기, Time: 29.78 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,10 +1,5 @@
 def solution(n, left, right):
     answer = []
     for i in range(left,right+1):
-        a = i//n # 몫
-        b = i%n #나머지 
-        if a<b: 
-            a,b =b,a 
-        answer.append(a+1)
-    
+        answer.append(max(i//n,i%n)+1)
     return answer
