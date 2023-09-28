7e2496b857f9792262decb833f1942c3e0b077a6
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-09-28 20:39:04 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,18 +1,22 @@
-def bracket(s):
-        stack = []
-        for i in s:
-            if len(stack) == 0: stack.append(i)
-            else:
-                if i == ")" and stack[-1] == "(": stack.pop()
-                elif i == "]" and stack[-1] == "[": stack.pop()
-                elif i == "}" and stack[-1] == "{": stack.pop()
-                else: stack.append(i)
-        return 1 if len(stack) == 0 else 0
-        
+def is_valid(s):
+    stack = []
+    for ch in s:
+        if not stack:
+            stack.append(ch)
+        elif stack[-1] == '(':
+            if ch==')': stack.pop()
+            else: stack.append(ch)
+        elif stack[-1] == '{':
+            if ch=='}': stack.pop()
+            else: stack.append(ch)
+        elif stack[-1] == '[':
+            if ch==']': stack.pop()
+            else: stack.append(ch)
+
+    return False if stack else True
+
 def solution(s):
     answer = 0
-    
     for i in range(len(s)):
-        if bracket(s): answer +=1
-        s = s[1:] + s[:1]
+        answer += is_valid(s[i:]+s[:i])
     return answer
