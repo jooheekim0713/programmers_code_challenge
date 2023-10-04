f9502487fd3f8b911345c4839e60f492fb3c0717
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-10-04 14:33:49 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,6 +1,24 @@
-import string
+def conv(n,k):
+    s = ''
+    while n:
+        s += str(n%k)
+        n //= k
+    return s[::-1]
+
+def isPrime(n):
+    if n <=1 : return False
+    i = 2
+    while i*i <= n:
+        if n %i == 0 : return False
+        i += 1
+    return True
 
 def solution(n, k):
-    number = string.digits + string.ascii_uppercase
-    q,r = divmod(n,k)
-    return solution(q,k) + number[r] if q else number[r]
+    s = conv(n,k)
+    answer = 0
+    
+    for num in s.split('0'):
+        if not num : continue
+        if isPrime(int(num)):
+            answer +=1    
+    return answer
