47cf0a08f0f02e4bd6390bcf343f4b0d6525a3dd
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-10-18 13:31:44 +0900

    [level 2] Title: 오픈채팅방, Time: 103.97 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,17 +1,14 @@
 def solution(record):
     answer = []
-    dic = {}
-    
-    for sentence in record:
-        sentence_split = sentence.split()
-        if len(sentence_split) == 3:
-            dic[sentence_split[1]] = sentence_split[2]
-            
-    for sentence in record:
-        sentence_split = sentence.split()
-        if sentence_split[0] == 'Enter':
-            answer.append('%s님이 들어왔습니다.' %dic[sentence_split[1]])
-        elif sentence_split[0] == 'Leave':
-            answer.append('%s님이 나갔습니다.' %dic[sentence_split[1]])
-            
-    return(answer)
+    namespace = {}
+    printer = {'Enter':'님이 들어왔습니다.', 'Leave':'님이 나갔습니다.'}
+    for r in record:
+        rr = r.split(' ')
+        if rr[0] in ['Enter', 'Change']:
+            namespace[rr[1]] = rr[2]
+
+    for r in record:
+        if r.split(' ')[0] != 'Change':
+            answer.append(namespace[r.split(' ')[1]] + printer[r.split(' ')[0]])
+
+    return answer
