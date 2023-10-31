7907527af00b1451df34a1332fdb2a3de1222405
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-01 08:43:09 +0900

    [level 2] Title: 택배상자, Time: 284.22 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,26 +1,12 @@
-from collections import deque
- 
 def solution(order):
-    order = deque(order)
-    main_line = deque(range(order[0] ,len(order) + 1))
-    sub_line = list(range(1, order[0]))
-    cnt = 0
-    flag = True
-    
-    while flag and order:
-        order_now = order.popleft()
-        while True:
-            if main_line and main_line[0] == order_now:
-                main_line.popleft()
-                cnt += 1
-                break
-            if sub_line and sub_line[-1] == order_now:
-                sub_line.pop()
-                cnt += 1
-                break
-            if not main_line:
-                flag = False
-                break
-            else:
-                sub_line.append(main_line.popleft())
-    return cnt
+    answer = 0
+
+    stack = []
+    for idx, num in enumerate(order):
+        stack.append(idx+1)
+
+        while stack and stack[-1] == order[answer]:
+            stack.pop()
+            answer +=1
+
+    return answer
