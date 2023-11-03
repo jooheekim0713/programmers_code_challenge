6258e2c7c060ec1f075aaf81253f84451dbf7e99
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-03 18:43:35 +0900

    [level 2] Title: 삼각 달팽이, Time: 2064.36 ms, Memory: 45.1 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,23 +1,19 @@
-import itertools
-
-def snailNext(x,y,d_snail) :
-    if d_snail %3 ==0 : x+=1
-    elif d_snail %3 ==1 : 
-        y+=1
-    else : 
-        x-=1
-        y -=1
-    return x,y 
-
 def solution(n):
-    tri_snail = [[0 for k in range(1,i+1)] for i in range(1,n+1) ]
-    direct_snail = range(n)  
-    x, y= -1, 0
-    idx = 1
-    
-    for d_snail in direct_snail :  
-        for i in range(d_snail, n) :
-            x,y = snailNext(x,y,d_snail)
-            tri_snail[x][y]= idx
-            idx+=1
-    return list(itertools.chain(*tri_snail))
+    dx=[0,1,-1]
+    dy=[1,0,-1]
+    b=[[0]*i for i in range(1,n+1)]
+    x,y=0,0
+    num=1
+    d=0
+    while num<=(n+1)*n//2:
+        b[y][x]=num
+        ny=y+dy[d]
+        nx=x+dx[d]
+        num+=1
+        if 0<=ny<n and 0<=nx<=ny and b[ny][nx]==0:
+            y,x=ny,nx
+        else:
+            d=(d+1)%3
+            y+=dy[d]
+            x+=dx[d]
+    return sum(b,[])
