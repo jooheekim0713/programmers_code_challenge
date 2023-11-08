a802ae0ce1ca6842d7548990c71eb61b63ba2716
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-08 14:08:57 +0900

    [level 2] Title: 주식가격, Time: 18.64 ms, Memory: 17.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,15 +1,11 @@
-from collections import deque
-
 def solution(prices):
-    queue = deque(prices)
-    answer = []
+    length = len(prices)
+    answer = [ i for i in range (length - 1, -1, -1)]
     
-    while queue:
-        price = queue.popleft()
-        sec = 0
-        for q in queue:
-            sec += 1
-            if price > q:
-                break 
-        answer.append(sec)        
+    stack = [0]
+    for i in range (1, length, 1):
+        while stack and prices[stack[-1]] > prices[i]:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
     return answer
