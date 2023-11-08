e3d4152a1e812da350515e07071d705803a3b42f
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-08 22:03:36 +0900

    [level 1] Title: 공원 산책, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -1,41 +1,20 @@
 def solution(park, routes):
-    # 위치 index
-    x = 0
-    y = 0 
-    
-    for i in range(len(park)):
-        for j in range(len(park[i])):
-            if park[i][j] == 'S':
-                x = j
-                y = i
+    W = len(park[0])
+    park = [['X']*(W+2)] + [[*'X'+i+'X'] for i in park] + [['X']*(W+2)]
+
+    x,y = 1,0
+    while park[x][y]!='S':
+        y += 1
+        if y>W:
+            x,y = x+1,0
+
+    delta = {k:v for k,v in zip('NEWS',[(-1,0),(0,1),(0,-1),(1,0)])}
+    for i in routes:
+        v,d = i.split()
+        for k in range(1,int(d)+1):
+            X,Y = x+k*delta[v][0], y+k*delta[v][1]
+            if park[X][Y]=='X':
                 break
-    
-    # 이동
-    for route in routes:
-        # 위치 초기화
-        xx = x
-        yy = y
-        # 이동 - 장애물이 있거나 공원을 벗어나면 명령 무시
-        for step in range(int(route[2])):
-            # 동쪽 : 현재 위치가 map 가장 오른쪽이면 안됨, 이동할 곳이 장애물이면 안됨
-            if route[0] == 'E' and xx != len(park[0])-1 and park[yy][xx+1] != 'X':
-                xx += 1
-                if step == int(route[2])-1:
-                    x = xx # step만큼 움직였으면 위치 초기화
-            # 서쪽 : 현재 위치가 map 가장 왼쪽이면 안됨, 이동할 곳이 장애물이면 안됨
-            elif route[0] == 'W' and xx != 0 and park[yy][xx-1] != 'X':
-                xx -= 1
-                if step == int(route[2])-1:
-                    x = xx
-            # 남쪽 : 현재 위치가 map 가장 아래쪽이면 안됨, 이동할 곳이 장애물이면 안됨
-            elif route[0] == 'S' and yy != len(park)-1 and park[yy+1][xx] != 'X':
-                yy += 1
-                if step == int(route[2])-1:
-                    y = yy
-            # 북쪽 : 현재 위치가 map 가장 위쪽이면 안됨, 이동할 곳이 장애물이면 안됨
-            elif route[0] == 'N' and yy != 0 and park[yy-1][xx] != 'X':
-                yy -= 1
-                if step == int(route[2])-1:
-                    y = yy
-                    
-    return [y, x]
+        else:
+            x,y = X,Y
+    return [x-1,y-1]
