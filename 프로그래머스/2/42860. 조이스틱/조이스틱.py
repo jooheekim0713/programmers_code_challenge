5663ffc0fa875a0d5b02e02ac391f6634c13c5e6
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-09 21:14:18 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,15 +1,21 @@
 def solution(name):
     answer = 0
-    min_move = len(name) - 1
-    
-    for i, char in enumerate(name):
-        answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
-        
-        next = i + 1
-        while next < len(name) and name[next] == 'A':
-            next += 1
-            
-        min_move = min([min_move, 2 *i + len(name) - next, i + 2 * (len(name) -next)])
-        
-    answer += min_move
+    n = len(name)
+
+    def alphabet_to_num(char):
+        num_char = [i for i in range(14)] + [j for j in range(12, 0, -1)]
+        return num_char[ord(char) - ord('A')]
+
+    for ch in name:
+        answer += alphabet_to_num(ch)
+
+    move = n - 1
+    for idx in range(n):
+        next_idx = idx + 1
+        while (next_idx < n) and (name[next_idx] == 'A'):
+            next_idx += 1
+        distance = min(idx, n - next_idx)
+        move = min(move, idx + n - next_idx + distance)
+
+    answer += move
     return answer
