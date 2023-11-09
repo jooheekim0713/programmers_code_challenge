bb6b0471f5062c4ee3c8b046fd9ef5a3ec709be1
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-09 14:15:23 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,7 +1,14 @@
-from collections import Counter
-from functools import reduce
-
 def solution(clothes):
-    cnt = Counter([kind for name, kind in clothes])
-    answer = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
-    return answer
+    clothes_type = {}
+
+    for c, t in clothes:
+        if t not in clothes_type:
+            clothes_type[t] = 2
+        else:
+            clothes_type[t] += 1
+
+    cnt = 1
+    for num in clothes_type.values():
+        cnt *= num
+
+    return cnt - 1
