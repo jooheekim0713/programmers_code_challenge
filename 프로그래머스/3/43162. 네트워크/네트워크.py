d040d1a31a3943aa09e60f41bd6dc22d50182af0
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-10 19:17:21 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,15 +1,16 @@
-def solution(n, computers):
+def solution(n, computers):            
+    
+    def DFS(i):
+        visited[i] = 1
+        for a in range(n):
+            if computers[i][a] and not visited[a]:
+                DFS(a)      
+                
     answer = 0
-    visited = [False for i in range(n)]
-    for com in range(n):
-        if visited[com] == False:
-            DFS(n, computers, com, visited)
-            answer += 1 #DFS로 최대한 컴퓨터들을 방문하고 빠져나오게 되면 그것이 하나의 네트워크.
+    visited = [0 for i in range(len(computers))]
+    for i in range(n):
+        if not visited[i]:
+            DFS(i)
+            answer += 1
+        
     return answer
-
-def DFS(n, computers, com, visited):
-    visited[com] = True
-    for connect in range(n):
-        if connect != com and computers[com][connect] == 1: #연결된 컴퓨터
-            if visited[connect] == False:
-                DFS(n, computers, connect, visited)
