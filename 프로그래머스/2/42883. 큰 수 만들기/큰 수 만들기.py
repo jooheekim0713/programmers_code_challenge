01cd1200c50e49e3abaacf18a7e824050e3357c7
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-10 14:14:14 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,10 +1,13 @@
 def solution(number, k):
-    answer = [] 
-    
-    for num in number:
-        while k > 0 and answer and answer[-1] < num:
-            answer.pop()
+    stack = []
+    for n in number:
+        while stack and stack[-1] < n and k > 0:
+            stack.pop()
             k -= 1
-        answer.append(num)
-        
-    return ''.join(answer[:len(answer) - k])
+        stack.append(n)
+
+    # 아직 제거되지 못 한 숫자를 뒤에서 삭제
+    if k > 0:
+        stack = stack[:-k]
+
+    return ''.join(stack)
