d98a6861b46d96387ba2c6b703af2eae80688ae9
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-10 14:58:02 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,17 @@
+def solution(n, costs):
+    answer = 0
+    costs.sort(key = lambda x: x[2]) 
+    link = set([costs[0][0]])
+    print(link)
+
+    # Kruskal 알고리즘으로 최소 비용 구하기
+    while len(link) != n:
+        for v in costs:
+            if v[0] in link and v[1] in link:
+                continue
+            if v[0] in link or v[1] in link:
+                link.update([v[0], v[1]])
+                answer += v[2]
+                break
+                
+    return answer
