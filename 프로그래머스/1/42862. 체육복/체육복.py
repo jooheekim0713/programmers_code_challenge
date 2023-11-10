708c23d32f92e5bc2f736f216852fad8f40de75e
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-10 11:12:26 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -3,6 +3,7 @@ def solution(n, lost, reserve):
     reserve_del = set(reserve)-set(lost)
     lost_del = set(lost)-set(reserve) 
     
+    print(set(reserve))
     for i in reserve_del: 
         if i-1 in lost_del: 
             lost_del.remove(i-1) 
