18614e357b9b04988a8d6fdec85fd81e65314274
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-23 12:13:39 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -1,19 +1,17 @@
 def solution(arr):
-    idx = 0
-    prev = arr
-    
-    while True:
-        change = []
-        for i in prev:
-            if i >= 50 and i % 2 == 0: change.append(int(i / 2))
-            elif i < 50 and i % 2 == 1: change.append(i * 2 + 1)
-            else: change.append(i)
-
-        same = all(a == b for a, b in zip(prev, change))
-        if same:
+    answer = 0
+    old = arr
+    while(True):
+        new = []
+        for i in old:
+            if i>=50 and i%2 == 0:
+                i = i/2
+            elif i<50 and i%2 == 1:
+                i = i*2 + 1
+            new.append(int(i))
+        if old == new:
             break
-        idx += 1
-
-        prev = change
-    
-    return idx
+        else:
+            old = new
+            answer += 1
+    return answer
