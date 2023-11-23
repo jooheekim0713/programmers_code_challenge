c657f5dea222c863c63da58c657701efd3d74f9d
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-23 18:08:32 +0900

    [level unrated] Title: 그림 확대, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.py
@@ -1,13 +1,6 @@
 def solution(picture, k):
     answer = []
-    
-    for row in picture: # 이미지의 한 줄을 가져온다.
-        resized = ''
-        
-        for pixel in row:
-            resized += pixel * k # 한 픽셀을 k배 만큼 가로로 늘린다.
-        
+    for i in range(len(picture)):
         for _ in range(k):
-            answer.append(resized) # 가로로 늘려진 이미지 한 줄을 k배 만큼 세로로 늘린다. 
-    
+            answer.append(picture[i].replace('.', '.' * k).replace('x', 'x' * k))
     return answer
