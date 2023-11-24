1d2efd9bed2f14592f3ff37c187b10da0507405a
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-24 12:37:36 +0900

    [level unrated] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/unrated/120883. 로그인 성공？/로그인 성공？.py
@@ -1,10 +1,4 @@
 def solution(id_pw, db):
-    answer = 'fail'
-    for d in db:
-        id,pw = d
-        if id == id_pw[0] and pw == id_pw[1]:
-            answer = 'login'
-        elif id == id_pw[0] and pw !=id_pw[1]:
-            answer = 'wrong pw'
-            
-    return answer
+    if db_pw := dict(db).get(id_pw[0]):
+        return "login" if db_pw == id_pw[1] else "wrong pw"
+    return "fail"
