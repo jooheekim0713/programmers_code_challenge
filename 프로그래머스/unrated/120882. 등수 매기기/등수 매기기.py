599ad72e1ac9ab47109f17dce808dc30e274ace1
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-24 12:59:49 +0900

    [level unrated] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.py
@@ -0,0 +1,8 @@
+def solution(score):
+    rank = sorted([sum(s) / 2 for s in score], reverse=True)
+    rankDict = {}
+    
+    for i, r in enumerate(rank):
+        if r not in rankDict.keys():
+            rankDict[r] = i + 1
+    return [rankDict[sum(s) / 2] for s in score]
