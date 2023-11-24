5583306aef4bab509607693af6d9853f5efa6a15
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-24 21:42:04 +0900

    [level 1] Title: 소수 찾기, Time: 208.25 ms, Memory: 64.4 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,7 +1,6 @@
 def solution(n):
-    num=set(range(2,n+1))
-
-    for i in range(2,n+1):
-        if i in num:
-            num-=set(range(2*i,n+1,i))
-    return len(num)
+    a=set(range(3,n+1,2))
+    for i in range(3,n+1,2):
+        if i in a:
+            a-=set(range(i*2,n+1,i))
+    return len(a)+1
