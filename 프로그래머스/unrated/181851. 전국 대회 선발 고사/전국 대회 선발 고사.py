81c947ddc7789b5059ec6047849312f6e4c17982
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-11-24 13:18:51 +0900

    [level unrated] Title: 전국 대회 선발 고사, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,12 +1,3 @@
 def solution(rank, attendance):
-    n = len(rank)
-    answer =0
-    r_a = []
-
-    for i in range(n):
-        if attendance[i]:
-            r_a.append([rank[i], i])
-
-    r_a.sort(key = lambda v : v[0])
-
-    return 10000 * r_a[0][1] + 100 * r_a[1][1] + r_a[2][1]
+    arr = sorted([(x, i) for i, x in enumerate(rank) if attendance[i]])
+    return arr[0][1] * 10000 + arr[1][1] * 100 + arr[2][1]
