9a18634375ed8ee08b7d68060ac76b617c461e28
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2023-12-19 20:47:38 +0900

    [level unrated] Title: OX퀴즈, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.py
@@ -1,12 +1,6 @@
-def solution(quiz):
-    answer = []
-    for q in quiz:
-        L,R = q.split(' = ')
-        a,op,b = L.split()
-        if op=='+':
-            result = 'O' if int(a)+int(b)==int(R) else 'X'
-            answer.append(result)
-        else:
-            result = 'O' if int(a)-int(b)==int(R) else 'X'
-            answer.append(result)
-    return answer
+def valid(equation):
+    equation = equation.replace('=', '==')
+    return eval(equation)
+
+def solution(equations):
+    return ["O" if valid(equation) else "X" for equation in equations]
--- 프로그래머스/unrated/120907. OX퀴즈/README.md
+++ 프로그래머스/unrated/120907. OX퀴즈/README.md
@@ -1,10 +1,10 @@
 # [level unrated] OX퀴즈 - 120907 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.02 ms
+메모리: 10.1 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 5일 17:6:29
+2023년 12월 2일 20:47:36
 
 ### 문제 설명
 
