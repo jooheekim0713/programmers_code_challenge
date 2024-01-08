eb616a3ee9ee611df5b46a70c11a386eb5c21f08
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2024-01-08 20:34:23 +0900

    [level 1] Title: 추억 점수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,10 +1,2 @@
 def solution(name, yearning, photo):
-    dictionary = dict(zip(name,yearning))
-    scores = []
-    for pt in photo:
-        score = 0
-        for p in pt:
-            if p in dictionary:
-                score += dictionary[p]
-        scores.append(score)
-    return scores
+    return [sum(yearning[name.index(j)] for j in i if j in name) for i in photo]
