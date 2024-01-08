87c5929f4686fbad1594c007f54c8f1fb75eba4a
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2024-01-08 14:19:23 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,2 @@
 def solution(array, commands):
-    answer = []
-    for comm in commands:
-        list_a = sorted(array[comm[0]-1:comm[1]])
-        answer.append(list_a[comm[2]-1])
-    return answer
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
