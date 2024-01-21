7c84aa23279c94da8c343697f8d22651eed87e07
Joohee Kim <103260346+jooheekim0713@users.noreply.github.com>
2024-01-21 18:04:50 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.56 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,51 +1,40 @@
 def solution(numbers, hand):
     answer = ''
-	
-    #키패드 위치
-    pad = {'1':(0,0), '2':(0,1), '3':(0,2),
-           '4':(1,0), '5':(1,1), '6':(1,2),
-           '7':(2,0), '8':(2,1), '9':(2,2),
-           '*':(3,0), '0':(3,1), '#':(3,2)
-        }
-    
-    left = pad['*']   #처음 왼손의 위치
-    right = pad['#']  #처음 오른손의 위치
-    
-    for num in numbers :
-    	#왼손이 누를 번호
-        if num == 1 or num == 4 or num == 7 :
-            answer += 'L'          #answer에 'L' 저장
-            left = pad[str(num)]   #해당 번호를 눌렀을 때 왼손의 위치 저장
-        
-        #오른손이 누를 번호
-        elif num == 3 or num == 6 or num == 9 :
-            answer += 'R'            #answer에 'R' 저장
-            right = pad[str(num)]    #해당 번호를 눌렀을 때 오른손의 위치 저장
-        
-        #번호가 2,5,8,0일 때
-        else :
-        	#번호와 왼손의 거리 계산
-            left_dis = abs(left[0] - pad[str(num)][0]) + abs(left[1] - pad[str(num)][1])
-            #번호와 오른손의 거리 계산
-            right_dis = abs(right[0] - pad[str(num)][0]) + abs(right[1] - pad[str(num)][1])
-            
-            #왼손이 더 가까울 때
-            if left_dis < right_dis :
+    key_dict = {1:(0,0),2:(0,1),3:(0,2),
+                4:(1,0),5:(1,1),6:(1,2),
+                7:(2,0),8:(2,1),9:(2,2),
+                '*':(3,0),0:(3,1),'#':(3,2)}
+
+    left = [1,4,7]
+    right = [3,6,9]
+    lhand = '*'
+    rhand = '#'
+    for i in numbers:
+        if i in left:
+            answer += 'L'
+            lhand = i
+        elif i in right:
+            answer += 'R'
+            rhand = i
+        else:
+            curPos = key_dict[i]
+            lPos = key_dict[lhand]
+            rPos = key_dict[rhand]
+            ldist = abs(curPos[0]-lPos[0]) + abs(curPos[1]-lPos[1])
+            rdist = abs(curPos[0]-rPos[0]) + abs(curPos[1]-rPos[1])
+
+            if ldist < rdist:
                 answer += 'L'
-                left = pad[str(num)]
-            
-            #오른손이 더 가까울 때
-            elif left_dis > right_dis :
+                lhand = i
+            elif ldist > rdist:
                 answer += 'R'
-                right = pad[str(num)]
-            
-            #왼손과 오른손 거리가 같을 때
-            else :
-                if hand == 'right' :
-                    answer += 'R'
-                    right = pad[str(num)]
-                else :
+                rhand = i
+            else:
+                if hand == 'left':
                     answer += 'L'
-                    left = pad[str(num)]
-    
+                    lhand = i
+                else:
+                    answer += 'R'
+                    rhand = i
+
     return answer
